da250ccb9c2c6584688a465d8a479fb949cf0574
Amaro <amarogarciasuarez@gmail.com>
2019-11-26 16:32:47 +0100

    sdfs

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 
 
 def lung_segmentation(imagen, p1, p2):
-    """Este método recibe una array N-dimensional (imagen) y dos puntos (x,y,z)"""
+    """Este método recibe una array N-dimensional (imagen) y dos puntos (x,y,z)."""
     pass
 
 def holaAmaro():
