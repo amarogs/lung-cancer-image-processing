4b0b739f1a90e1ceb3e69ad72802fcb22d8a79bf
Amaro García Suárez <amagarsua@alum.us.es>
2019-12-05 11:46:41 +0100

    visualizacion

--- funciones.py
+++ funciones.py
@@ -1,6 +1,19 @@
-import numpy as np
-import SimpleITK as sitk
-import matplotlib.pyplot as plt
+import skimage
+import pydicom
+import os
+from glob import glob
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import scipy.ndimage
+from skimage import morphology
+from skimage import measure
+from skimage.transform import resize
+from sklearn.cluster import KMeans
+from plotly import _version_
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
+
+
 
 
 
@@ -51,9 +64,88 @@ def mostrar_slice(image_sitk):
     new_array = np.array(np.swapaxes(img, 0, 2))
     new_array = np.array(np.swapaxes(new_array, 0, 1))
     
-    #plt.imshow(new_array[:, :, new_array.shape[2]//2], cmap="gray")
-    plt.imshow(new_array[new_array.shape[0]//2, :, :], cmap="gray")
+    plt.imshow(new_array[:, :, new_array.shape[2]//2], cmap="gray")
+    #plt.imshow(new_array[new_array.shape[0]//2, :, :], cmap="gray")
     
+    plt.show()
+
+
+def obtener_array(imagen_sitk):
+    """Dada una imagen itk la transformamos a np array y colocamos
+    las componentes de manera adecuada. """
+    img = sitk.GetArrayFromImage(imagen_sitk)
+    new_array = np.array(np.swapaxes(img, 0, 2))
+    new_array = np.array(np.swapaxes(new_array, 0, 1))
+
+    return new_array
+
+#De aqui hacia abajotodo lo nuevo para dibujar
+
+
+def resample(image, dim,  new_spacing=[1, 1, 1]):
+    # Determine current pixel spacing
+    spac = list(image.GetSpacing()[:2])
+    spacing = map(float, (dim + spac))
+    spacing = np.array(list(spacing))
+
+    img_arr = sitk.GetArrayFromImage(image)
+    resize_factor = spacing / new_spacing
+    new_real_shape = img_arr.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / img_arr.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(img_arr, real_resize_factor)
+
+    return image, new_spacing
+
+
+def make_mesh(image, threshold=-600, step_size=1):
+    print("Transposing surface")
+    p = image.transpose(2, 1, 0)
+
+    print("Calculating surface")
+    verts, faces, norm, val = measure.marching_cubes_lewiner(
+        p, threshold, step_size=step_size, allow_degenerate=True)
+    return verts, faces
+
+
+def plotly_3d(verts, faces):
+    x, y, z = zip(*verts)
+
+    print("Drawing")
+
+    # Make the colormap single color since the axes are positional not intensity.
+    #    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+    colormap = ['rgb(236, 236, 212)', 'rgb(236, 236, 212)']
+
+    fig = FF.create_trisurf(x=x,
+                            y=y,
+                            z=z,
+                            plot_edges=False,
+                            colormap=colormap,
+                            simplices=faces,
+                            backgroundcolor='rgb(64, 64, 64)',
+                            title="Interactive Visualization")
+    iplot(fig)
+
+
+def plt_3d(verts, faces):
+    print("Drawing")
+    x, y, z = zip(*verts)
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+    face_color = [1, 1, 0.9]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0, max(y))
+    ax.set_zlim(0, max(z))
+    ax.set_facecolor((0.7, 0.7, 0.7))
     plt.show()
 
 
--- pruebas2.py
+++ pruebas2.py
@@ -4,9 +4,11 @@ import SimpleITK as sitk
 import pandas as pd
 import funciones
 import pydicom
+import vtk
+import volume_rendering
 
 #Leemos la imagen en formato itk
-img = funciones.leer_dicom("./database/2/")
+img = funciones.leer_dicom("./database/0/")
 
 #funciones.mostrar_slice(img_re)
 funciones.mostrar_slice(img)
@@ -23,22 +25,30 @@ funciones.mostrar_slice(img)
 ws = sitk.MorphologicalWatershed(sitk.GradientMagnitude(img), markWatershedLine=True, level=7)
 funciones.mostrar_slice(ws)
 
-stats = sitk.LabelShapeStatisticsImageFilter()
-stats.Execute(sitk.ConnectedComponent(ws))
+# stats = sitk.LabelShapeStatisticsImageFilter()
+# stats.Execute(sitk.ConnectedComponent(ws))
 
-stats_list = [ (stats.GetPhysicalSize(i),
-               stats.GetElongation(i),
-               stats.GetFlatness(i),
-               stats.GetOrientedBoundingBoxSize(i)[0],
-               stats.GetOrientedBoundingBoxSize(i)[2]) for i in stats.GetLabels()]
+# stats_list = [ (stats.GetPhysicalSize(i),
+#                stats.GetElongation(i),
+#                stats.GetFlatness(i),
+#                stats.GetOrientedBoundingBoxSize(i)[0],
+#                stats.GetOrientedBoundingBoxSize(i)[2]) for i in stats.GetLabels()]
 
-cols = ["Volume (nm^3)",
-        "Elongation",
-        "Flatness",
-        "Oriented Bounding Box Minimum Size(nm)",
-        "Oriented Bounding Box Maximum Size(nm)"]
+# cols = ["Volume (nm^3)",
+#         "Elongation",
+#         "Flatness",
+#         "Oriented Bounding Box Minimum Size(nm)",
+#         "Oriented Bounding Box Maximum Size(nm)"]
 
-stats = pd.DataFrame(
-    data=stats_list, index=stats.GetLabels(), columns=cols)
+# stats = pd.DataFrame(
+#     data=stats_list, index=stats.GetLabels(), columns=cols)
 
-stats
+# stats
+
+
+#El thikness en este caso es 3 y spacing= 0,781
+imgs_after_resamp, spacing = funciones.resample(
+    img, [img.GetDimension()], [1, 1, 1])
+img_arr = sitk.GetArrayFromImage(img)
+v, f = funciones.make_mesh(img_arr, 350)
+funciones.plt_3d(v, f)
