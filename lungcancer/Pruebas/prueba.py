15c95713c79fad6fc81d57354b5388e567a69e1b
Amaro <amarogarciasuarez@gmail.com>
2020-01-14 10:43:29 +0100

    exp

--- lungcancer/Pruebas/prueba.py
+++ lungcancer/Pruebas/prueba.py
@@ -5,7 +5,7 @@ import SimpleITK as sitk
 #Cargamos la imagen que deseamos leer
 img = lectura.leer_dicom("./QIN LUNG CT/QIN-LSC-0003/04-01-2015-1-CT Thorax wContrast-41946/2-THORAX W  3.0  B41 Soft Tissue-71225")
 
-funciones.mostrar_slice(img, 69)
+# funciones.mostrar_slice(img, 69)
 
 #Cargamos la mascara del nodulo
 img_nodulo = lectura.leer_una_imagen("./QIN LUNG CT/QIN-LSC-0003/04-01-2015-1-CT Thorax wContrast-41946/1000-QIN CT challenge alg01 run01segmentation")
@@ -16,4 +16,9 @@ img_nodulo.CopyInformation(img)
 seeds = [(100, 250, 65), (350, 350, 65)]
 
 img_segmentada = funciones.lung_segmentation(img, seeds)
-funciones.mostrar_slice(img_segmentada, 69)
+# funciones.mostrar_slice(img_segmentada, 69)
+
+# verts, faces = funciones.make_mesh(funciones.obtener_array(img), -800)
+# funciones.plotly_3d(verts, faces,)
+#ws = sitk.MorphologicalWatershed(img_segmentada, markWatershedLine=True, level=28, fullyConnected=False)
+# overlay = sitk.LabelToRGB(ws)
